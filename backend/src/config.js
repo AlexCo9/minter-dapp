2c00114f6b7aed01ef8eabb7c4a77acf669fcc13
Your Name~ <alex.cohen@katalyst.earth>
2022-05-25 20:46:29 +1000

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,10 +65,10 @@ const MINT_PRICE = 0.002; // Minting price per NFT. Rinkeby = ETH, Polygon = MAT
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-10-19T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-10-19T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-10-06T00:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-10-06T00:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x4FaF9432F7Dbfe93b67057Bb6aAdDEe6684DEB08"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
@@ -153,11 +153,11 @@ const preview = {
 };
 
 const preview_gif = {
-  numberOfImages: 25,
+  numberOfImages: 7,
   order: "ASC", // ASC, DESC, MIXED
   repeat: 0,
   quality: 100,
-  delay: 3000,
+  delay: 1000,
   imageName: "preview.gif",
 };
 
